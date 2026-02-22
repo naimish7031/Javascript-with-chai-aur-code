ec7714e6f996fecb87add64e827d8856b2159111
Naimish Kumar <naimishkumar7031@gmail.com>
2026-02-22 23:00:18 +0530

    Add more points

--- 01_basics/first.js
+++ 01_basics/first.js
@@ -1,1 +1,14 @@
+// what is Javascript
+
+// JavaScript is a high-level, dynamic programming language used to   interactive and functional content on websites.
+
+// Node.js provides an environment to run JavaScript programs.
+// Similarly, Deno.js also provides an environment to run JavaScript.
+
+
+// Note .  JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+
+
+
+
 console.log("Hi Naimish!");
